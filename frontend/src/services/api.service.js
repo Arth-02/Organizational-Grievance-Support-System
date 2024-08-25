f6f37969faa6299f27b5a8ff06b99352ae6a5c46
Kunj Detroja <kunjdetroja52@gmail.com>
2024-08-25 18:26:06 +0530

    Remove unused import in api.service.js

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { checkUsername } from "../../../backend/controllers/user.controller";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
