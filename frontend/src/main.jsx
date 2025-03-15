6f27d1b708154fba9a79cb5d68c62d3d72d54ffe
Arth Patel <arthmp2003@gmail.com>
2025-03-15 22:04:59 +0530

    Remove unnecessary whitespace in main.jsx for cleaner code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,6 @@ createRoot(document.getElementById("root")).render(
     <TooltipProvider>
       <BrowserRouter>
         <App />
-        
       </BrowserRouter>
       <Toaster position="top-right" />
     </TooltipProvider>
